dc57e3f58cac5dc1f02275f5ad2a0e03e82f0e6e
Dixon Johnson <dixon@Dixons-MacBook-Pro.local>
2023-11-27 16:27:56 -0700

    Final commit for MVP

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -1,12 +1,13 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ProductContext = createContext();
+const serverhost = import.meta.env.VITE_SERVERHOST;
 
 export const useProduct = () => useContext(ProductContext);
 export const ProductProvider = ({ children }) => {
     const [productlist , setProducts] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:8080/api/clothes')
+        fetch(`${serverhost}/api/clothes`)
             .then(response => response.json())
             .then(data => setProducts(data))
             .catch(error => console.error('Error fetching data: ', error));
