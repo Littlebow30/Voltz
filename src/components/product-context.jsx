3bfbca364916d1fb5965734ed94de33eb799b0cf
Littlebow30 <bowmanA1906@gmail.com>
2023-11-27 21:38:08 -0500

    front end render deployment

--- src/components/product-context.jsx
+++ src/components/product-context.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-
+const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
 const ProductContext = createContext();
 
 export const useProduct = () => useContext(ProductContext);
 export const ProductProvider = ({ children }) => {
     const [productlist , setProducts] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:8080/api/clothes')
+        fetch(`${apiUrl}/api/clothes`)
             .then(response => response.json())
             .then(data => setProducts(data))
             .catch(error => console.error('Error fetching data: ', error));
